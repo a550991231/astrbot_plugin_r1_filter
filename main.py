5bc4f9bd57fb6b9c86ec5f887b3887afc052b005
Camreishi <h15223449277@sina.com>
2025-02-07 18:12:36 +0800

    feat: 添加对Groq deepseek-r1-distill-llama-70b模型的支持 fix: 修正metadata.yaml

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api.provider import LLMResponse
 from openai.types.chat.chat_completion import ChatCompletion
 
-@register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0")
+@register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0", 'https://github.com/Soulter/astrbot_plugin_r1_filter')
 class R1Filter(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
@@ -16,10 +16,21 @@ class R1Filter(Star):
         if self.display_reasoning_text:
             if response and response.raw_completion and isinstance(response.raw_completion, ChatCompletion):
                 if len(response.raw_completion.choices) \
-                    and response.raw_completion.choices[0].message \
-                    and hasattr(response.raw_completion.choices[0].message, 'reasoning_content'):
+                        and response.raw_completion.choices[0].message:
                     message = response.raw_completion.choices[0].message
-                    response.completion_text = f"🤔思考：{message.reasoning_content}\n\n{message.content}"
+                    reasoning_content = ""  # 初始化 reasoning_content
+
+                    # 检查 Groq deepseek-r1-distill-llama-70b模型的 'reasoning' 属性
+                    if hasattr(message, 'reasoning') and message.reasoning:
+                        reasoning_content = message.reasoning
+                    # 检查 DeepSeek deepseek-reasoner模型的 'reasoning_content'
+                    elif hasattr(message, 'reasoning_content') and message.reasoning_content:
+                        reasoning_content = message.reasoning_content
+
+                    if reasoning_content:
+                        response.completion_text = f"🤔思考：{reasoning_content}\n\n{message.content}"
+                    else:
+                        response.completion_text = message.content
                     
         else: 
             # DeepSeek 官方的模型的思考存在了 reason_content 字段因此不需要过滤
