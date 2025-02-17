111a467fc8fa181c1e39ec97b806bce5be689a1c
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-17 20:40:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ class R1Filter(Star):
         self.display_reasoning_text = self.config.get('display_reasoning_text', True)
     
     @filter.on_llm_response()
-   async def resp(self, event: AstrMessageEvent, response: LLMResponse):
-       details_start = '<details style="color:gray;background-color: #f8f8f8;padding: 8px;border-radius: 4px;" open> <summary> Thinking... </summary>'
-       details_end = '</details>'
-
-    # 删除 <details> 标签及其内容
-        if details_start in response.completion_text and details_end in response.completion_text:
-            start_index = response.completion_text.find(details_start)
-            end_index = response.completion_text.find(details_end) + len(details_end)
-            response.completion_text = response.completion_text[:start_index] + response.completion_text[end_index:].strip()
+   async def resp(self, event: AstrMessageEvent, response: LLMResponse)：
+    details_start = '<details style="color:gray;background-color: #f8f8f8;padding: 8px;border-radius: 4px;" open> <summary> Thinking... </summary>'
+    details_end = '</details>'
+    if details_start in response.completion_text and details_end in response.completion_text:
+        start_index = response.completion_text.find(details_start)
+        end_index = response.completion_text.find(details_end) + len(details_end)
+        response.completion_text = response.completion_text[:start_index] + response.completion_text[end_index:].strip()
