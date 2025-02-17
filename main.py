d3359e2f903bdc881f994ef5c7136081c3769913
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-17 18:23:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,12 +33,11 @@ class R1Filter(Star):
   #      msg = response.completion_text
   #      response.completion_text = self._remove_details_filter(msg) 
        # event.send(response.completion_text
-  @filter.on_llm_response()
-  async def on_llm_resp(self, event: AstrMessageEvent):  
-      result = event.get_result()
-      chain = self._remove_details_filter(result.chain)
-      event.send(chain)
-      
+    @filter.on_decorating_result()
+    async def on_decorating_result(self, event: AstrMessageEvent):
+        result = event.get_result()
+        chain = self._remove_details_filter(result.chain)
+        
       
 
     
