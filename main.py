922f6b322602e622c0643206dfa768caa4248fdd
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-17 18:58:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class R1Filter(Star):
 
     @filter.on_llm_response()
     async def on_llm_resp(self, event: AstrMessageEvent, response: LLMResponse):
-        msg = "".join(response.completion_text)
+        msg = "".join(completion_text)
         if "<details" in msg:
             response.completion_text = self._remove_details_filter(msg)
 
