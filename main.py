a2738a6e7aed55f39cad4580eb977908f9bfa022
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-17 20:30:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api.provider import LLMResponse
 from openai.types.chat.chat_completion import ChatCompletion
 
+@register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0", 'https://github.com/Soulter/astrbot_plugin_r1_filter')
 class R1Filter(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
@@ -11,7 +12,7 @@ class R1Filter(Star):
         self.display_reasoning_text = self.config.get('display_reasoning_text', True)
     
     @filter.on_llm_response()
-    async def on_llm_resp(self, event: AstrMessageEvent, response: LLMResponse):
+    async def resp(self, event: AstrMessageEvent, response: LLMResponse):
         if self.display_reasoning_text:
             if response and response.raw_completion and isinstance(response.raw_completion, ChatCompletion):
                 if len(response.raw_completion.choices) \
@@ -34,11 +35,9 @@ class R1Filter(Star):
         else: 
             # DeepSeek 官方的模型的思考存在了 reason_content 字段因此不需要过滤
             completion_text = response.completion_text
-            details_start = '<details style="color:gray;background-color: #f8f8f8;padding: 8px;border-radius: 4px;" open> <summary> Thinking... </summary>'
-            details_end = '</details>'
-
-    # 删除 <details> 标签及其内容
-            if details_start in response.completion_text and details_end in response.completion_text:
-                start_index = response.completion_text.find(details_start)
-                end_index = resp.completion_text.find(details_end) + len(details_end)
-                response.completion_text = response.completion_text[:start_index] + response.completion_text[end_index:].strip()
+            # 适配 ollama deepseek-r1 模型
+            if r'<think>' in completion_text or r'</think>' in completion_text:
+                completion_text = re.sub(r'<think>.*?</think>', '', completion_text, flags=re.DOTALL).strip()
+                # 可能有单标签情况
+                completion_text = completion_text.replace(r'<think>', '').replace(r'</think>', '').strip()
+            response.completion_text = completion_text
