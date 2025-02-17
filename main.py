3f75cbe9e48e99fc803cb80bd32f30be0a53cbd8
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-17 20:33:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ class R1Filter(Star):
         self.display_reasoning_text = self.config.get('display_reasoning_text', True)
     
     @filter.on_llm_response()
-async def on_llm_resp(self, event: AstrMessageEvent, resp: LLMResponse):
+async def on_llm_resp(self, event: AstrMessageEvent, response: LLMResponse):
     # 定义要删除的 <details> 标签内容
     details_start = '<details style="color:gray;background-color: #f8f8f8;padding: 8px;border-radius: 4px;" open> <summary> Thinking... </summary>'
     details_end = '</details>'
 
     # 删除 <details> 标签及其内容
-    if details_start in resp.completion_text and details_end in resp.completion_text:
-        start_index = resp.completion_text.find(details_start)
-        end_index = resp.completion_text.find(details_end) + len(details_end)
-        resp.completion_text = resp.completion_text[:start_index] + resp.completion_text[end_index:].strip()
+    if details_start in response.completion_text and details_end in response.completion_text:
+        start_index = response.completion_text.find(details_start)
+        end_index = response.completion_text.find(details_end) + len(details_end)
+        response.completion_text = response.completion_text[:start_index] + response.completion_text[end_index:].strip()
