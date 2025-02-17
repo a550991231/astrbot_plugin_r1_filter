bc4f0acc999ef1ad92c2044d9cc9c00763c29f29
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-17 12:50:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api.provider import LLMResponse
 
-@register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0", 'https://github.com/Soulter/astrbot_plugin_r1_filter')
+@register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0", 'https://github.com/a550991231/astrbot_plugin_r1_filter')
 class R1Filter(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
