c3e155c150e64d48c94cc27aae0f30a2e3c9f5de
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-18 11:42:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,30 +14,4 @@ class R1Filter(Star):
     @filter.on_llm_response()
     async def resp(self, event: AstrMessageEvent, response: LLMResponse):
         if self.display_reasoning_text:
-            if response and response.raw_completion and isinstance(response.raw_completion, ChatCompletion):
-                if len(response.raw_completion.choices) \
-                        and response.raw_completion.choices[0].message:
-                    message = response.raw_completion.choices[0].message
-                    reasoning_content = ""  # 初始化 reasoning_content
-
-                    # 检查 Groq deepseek-r1-distill-llama-70b模型的 'reasoning' 属性
-                    if hasattr(message, 'reasoning') and message.reasoning:
-                        reasoning_content = message.reasoning
-                    # 检查 DeepSeek deepseek-reasoner模型的 'reasoning_content'
-                    elif hasattr(message, 'reasoning_content') and message.reasoning_content:
-                        reasoning_content = message.reasoning_content
-
-                    if reasoning_content:
-                        response.completion_text = f"🤔思考：{reasoning_content}\n\n{message.content}"
-                    else:
-                        response.completion_text = message.content
-                    
-        else: 
-            # DeepSeek 官方的模型的思考存在了 reason_content 字段因此不需要过滤
-            completion_text = response.completion_text
-            # 适配 ollama deepseek-r1 模型
-            if r'<details' in completion_text or r'</details>' in completion_text:
-                completion_text = re.sub(r'<details[^>]*>[\s\S]*?</details>', '', completion_text, flags=re.DOTALL).strip()
-                # 可能有单标签情况
-                completion_text += " 测试1231"
-            response.completion_text = completion_text
+            response.completion_text += " 测试1231"
