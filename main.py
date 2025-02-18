4701b5211e189796ec33dca9cef1f50333179e79
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-18 12:52:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api.provider import LLMResponse
-from openai.types.chat.chat_completion import ChatCompletion
+
 
 @register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0", 'https://github.com/Soulter/astrbot_plugin_r1_filter')
 class R1Filter(Star):
