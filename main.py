a06e79ef383ccd1d84cef0ca9c6eb473369e0ab6
a550991231 <76810085+a550991231@users.noreply.github.com>
2025-02-18 12:56:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ from astrbot.api.provider import LLMResponse
 
 
 @register("r1-filter", "Soulter", "可选择是否过滤推理模型的思考内容", "1.0.0", 'https://github.com/Soulter/astrbot_plugin_r1_filter')
-class R1Filter(Star):
-    def __init__(self, context: Context, config: dict):
-        super().__init__(context)
-        self.config = config
-        self.display_reasoning_text = self.config.get('display_reasoning_text', True)
+
     
-    @filter.on_llm_response()
-    async def resp(self, event: AstrMessageEvent, response: LLMResponse):
-        if self.display_reasoning_text:
-            response.completion_text = " 测试1231"
+@filter.on_llm_response()
+async def resp(self, event: AstrMessageEvent, response: LLMResponse):
+    response.completion_text = " 测试1231"
